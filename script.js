c097fd337dfd94b839bb363376a27828a79346b1
peter-conder <pconder2005@gmail.com>
2026-07-06 10:09:20 -0600

    make result based on variable, remove all comments

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 
-
-/* use random numbers to determine strings
-    GENERATE random number as a *random*
-    MULTIPLY by 100
-        IF *random*:
-            is zero to 33: set computerChoice as Rock
-                means 0 <= randomNumber <= 33
-            is 34 to 66: set computerChoice as Paper
-            is 67 to 100: set computerChoice as Scissors   */
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 100);
@@ -25,21 +16,6 @@ function getComputerChoice() {
 
 };
 
-/* So currently this function works perfectly. Right now it's printing the computer's choice to the console, whereas in the actual game I'm going to have to take that variable and input it into more functions. */
-
-//getComputerChoice();
-
-
-/* use a prompt to get a string. prompt input must be equal to string input.
-    Ok so for this I need to have a prompt pop up, where the user can input 'Rock', 'Paper' or 'Scissors.' Then, if what they input is equal to the strings rock, paper or scissors, the playerChoice variable will change to match. I think the easiest way to do this is to set up a simple if-else if-else statement.
-        PROMPT user
-        IF prompt:
-            is 'Rock': playerChoice = Rock
-            is 'Scissors': playerCHoice = Scissors
-            is 'Paper': playerChoice = Paper
-
-*/
-
 function getPlayerChoice() {
     let playerPrompt = prompt("What do you play?");
     if (playerPrompt === 'rock') {
@@ -51,66 +27,44 @@ function getPlayerChoice() {
     } else {return console.log('Invalid Choice! :c'); }
 }
 
-//getPlayerChoice();
 
-/*
-    Compare humanChoice and computerChoice and decide who wins the round
-
-    How do I actually get this to work? let's say that everything is handled from the human's point of view. So:
-        IF human plays 'Rock'
-            IF bot plays Rock, result is tie
-            ELSE IF bot plays Paper, result is loss
-            ELSE IF bot plays Scissors, result is win
-        ELSE IF human plays 'Paper'
-            IF bot plays Rock, result is win
-            ELSE IF bot plays Paper, result is tie
-            ELSE IF bot plays Scissors, result is loss
-        ELSE IF human plays 'Scissors'
-            IF bot plays Rock, result is loss
-            ELSE IF bot plays Paper, result is win
-            ELSE IF bot plays Scissors, result is tie
-        Print a string to the console
-        Increment humanScore or computerScore values 
-*/
 
 function playRound(playerChoice, computerChoice) {
-    //defines game logic using humanChoice and computerChoice
     let result;
     if (playerChoice === 'Rock') {
         if (computerChoice === 'Rock') {
             let result = 'tie';
-            return console.log("Tie!")
         } else if (computerChoice === 'Paper') {
             let result = 'loss';
-            return console.log('You lose!');
         } else if (computerChoice === 'Scissors') {
             let result = 'win';
-            return console.log('You win!');
         }
     } else if (playerChoice === 'Paper') {
         if (computerChoice === 'Rock') {
             let result = 'win';
-            return console.log("You win!")
         } else if (computerChoice === 'Paper') {
             let result = 'tie';
-            return console.log('Tie!');
         } else if (computerChoice === 'Scissors') {
             let result = 'lose';
-            return console.log('You lose!');
         }
 
     } else if (playerChoice === 'Scissors') {
         if (computerChoice === 'Rock') {
             let result = 'lose';
-            return console.log("You lose!")
         } else if (computerChoice === 'Paper') {
             let result = 'win';
-            return console.log('You win!');
         } else if (computerChoice === 'Scissors') {
             let result = 'tie';
-            return console.log('Tie!');
         }
+    }
 
+    if (result === 'win') {
+        return console.log("You win!");
+    } else if (result === 'lose') {
+        console.log(computerScore);
+        return console.log("You lose!");
+    } else if (result === 'tie') {
+        return console.log("You both picked the same thing. Why");
     }
 }
 
@@ -118,8 +72,3 @@ const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
 playRound(playerSelection, computerSelection);
-
-/* so currently nothing is happening when the computer and I make our selections. this might have to do with the variables that have been written, specifically their scope. I need to have playerChoice and computerChoice taken out of their respective functions and put into playRound. I need to declare playerChoice and computerChoice as global variables and then influence them with the functions. */
-
-/* Yes!! it's working now. However, I currently am inputting twice, which shouldn't be happening.
-Actually I think the whole script is running twice. hang on. I didn't need to actually use the Choice functions on their own because they are being called by playRound.*/
